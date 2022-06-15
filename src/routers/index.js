35b661218ec5efcd6e2855208d01c3a069aabb3a
RomanMiguel <mgroman96@gmail.com>
2022-06-15 20:29:56 -0300

    first commit

---
+++ src/routers/index.js
@@ -0,0 +1,5 @@
+
+export { default as routerCart } from "./routerCart";
+export { default as routerProducts } from "./routerProducts";
+export { default as routerUser } from "./routerUser";
+export { default as routerInfo } from "./routerInfo";
